b0150c09c16345f58c50d96c601ebfe664510a81
kuroChanh <tuyetnhi@hotmail.ca>
2018-04-06 19:10:43 -0400

    Menu button goes to gameplay when pressed

--- Swipe Brigade/GameViewController.swift
+++ Swipe Brigade/GameViewController.swift
@@ -11,15 +11,37 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView(){
+        let view = UIView()
+        view.backgroundColor = .white
+        view.sizeToFit()
+        //the background image ;w; to be changed
+        let image = UIImage(named: "shiba.png")
+        let imageV = UIImageView(image: image)
+        imageV.center = CGPoint(x: 200, y: 250)
+        view.addSubview(imageV)
+        //play button
+        let play = UIButton(type: .roundedRect) as UIButton
+        play.setTitle("PLAY", for: .normal)
+        play.setTitleColor(UIColor.white, for: .normal)
+        play.frame = CGRect(x: 135, y: 500, width: 150, height: 45)
+        play.backgroundColor = UIColor.darkGray
+        play.addTarget(self, action: #selector(playGame), for: .touchUpInside)
+        //play.center = view.center ;w; ask
+        view.addSubview(play)
+        //this will render everything onto the screen
+        self.view = view
+    }
+    @IBAction func playGame() {
+        print("HELLO")
         let scene = GameScene(size: CGSize(width: 2048, height: 1536))
-        let skView = self.view as! SKView
+        let skView = SKView()
         skView.showsFPS = true
         skView.showsNodeCount = true
         skView.ignoresSiblingOrder = true
         scene.scaleMode = .aspectFill
         skView.presentScene(scene)
+        self.view = skView
     }
     override var prefersStatusBarHidden: Bool {
         return true
